8b218f125b7cfa6318748d22bc89e59ed2a76c94
jordan.besly <jordan.besly@transatel.com>
2020-02-17 12:12:14 +0100

    Brief commentary about what the package does

--- with-shell-interpreter.el
+++ with-shell-interpreter.el
@@ -28,6 +28,9 @@
 ;;; Commentary:
 ;;  -----------
 ;;
+;; Helper macro for Emacs shell command APIs, making implict argument as explicit keyword arguments.
+;; Provides macro `with-shell-interpreter'.
+;;
 ;; For detailed instructions, please look at the README.md at https://github.com/p3r7/with-shell-interpreter/blob/master/README.md
 
 ;;; Code:
